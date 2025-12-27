40689280faad4f03aee5ca8afd60fa7de326b7ec
Bodoor <Bodoor5@hotmail.com>
2025-12-27 07:28:12 +0300

    update api.js

--- TasteTogether-Backend/TasteTogether-Frontend/src/services/api.js
+++ TasteTogether-Backend/TasteTogether-Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001' 
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
